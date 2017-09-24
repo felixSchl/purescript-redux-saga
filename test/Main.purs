6e3e0081646e5b330ac39966fc728ccf8104b585
Felix Schlitter <felixschlitter@protonmail.com>
2017-09-25 06:04:00 +1300

    Clean up imports

--- test/Main.purs
+++ test/Main.purs
@@ -2,25 +2,16 @@ module Test.Main where
 
 import Prelude
 import Data.Maybe (Maybe(..))
-import Data.Array as Array
 import Data.Time.Duration (Milliseconds(..))
-import Control.Monad.Trans.Class (lift)
-import Control.Monad.Aff (Aff, launchAff, delay)
+import Control.Monad.Aff (delay)
 import Control.Monad.Aff.Class (liftAff)
-import Control.Monad.Aff.Unsafe (unsafeCoerceAff)
-import Control.Monad.Aff.AVar (AVAR)
 import Control.Monad.Eff (Eff)
-import Control.Monad.Eff.Ref (newRef, modifyRef, REF, readRef)
-import Control.Monad.Eff.Unsafe (unsafeCoerceEff)
-import Control.Monad.Eff.Uncurried (mkEffFn1)
 import Control.Monad.Eff.Console as Console
 import Control.Monad.Eff.Console (CONSOLE)
-import Control.Monad.Eff.Exception (EXCEPTION)
 import Control.Monad.Eff.Class (liftEff)
 import React.Redux as Redux
-import React.Redux (REDUX, ReduxReactClass')
 
-import Redux.Saga (Saga, takeEvery, take, fork, put, joinTask, sagaMiddleware)
+import Redux.Saga
 
 data Action = SearchChanged String
 type GlobalState = {}
@@ -53,5 +44,5 @@ mkStore saga = Redux.createStore reducer initialState middlewareEnhancer
   middlewareEnhancer :: Redux.Enhancer _ Action GlobalState
   middlewareEnhancer = Redux.applyMiddleware  [ sagaMiddleware saga ]
 
-main :: ∀ eff. Eff _ Unit
+main :: Eff _ Unit
 main = void $ mkStore mySaga
